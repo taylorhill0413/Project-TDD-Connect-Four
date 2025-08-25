cac379941c3bc52c8203182ed9324d4b6f671615
Taylor Hill <taylorhill0413@gmail.com>
2025-08-25 15:03:26 +0700

    done board and board_spec

--- lib/board.rb
+++ lib/board.rb
@@ -1,9 +1,72 @@
 class Board
   attr_reader :grid
 
-  COLUMNS = 6
-  ROWS = 5
+  COLUMNS = 7
+  ROWS = 6
   def initialize
     @grid = Array.new(ROWS) { Array.new(COLUMNS, nil) }
+  end
+
+  def place_piece(column, symbol)
+    raise 'Column out of bounds' if column < 0 || column >= COLUMNS
+
+    row = check_column(column)
+    raise 'Column is full' if row.nil?
+
+    grid[row][column] = symbol
+    row
+  end
+
+  def check_column(column)
+    (ROWS - 1).downto(0).find { |row| grid[row][column].nil? }
+  end
+
+  def full?
+    grid.flatten.none?(&:nil)
+  end
+
+  def display
+    puts(grid.map { |row| row.map { |cell| cell || '-' }.join(' ') })
+    puts (0...COLUMNS).to_a.join(' ')
+  end
+
+  def winning_move?(symbol)
+    horizontal_win?(symbol) || vertical_win?(symbol) || diagonal_win?(symbol)
+  end
+
+  def horizontal_win?(symbol)
+    grid.any? do |row|
+      row.each_cons(4).any? { |group| group.all? { |cell| cell == symbol } }
+    end
+  end
+
+  def vertical_win?(symbol)
+    grid.transpose.any? do |row|
+      row.each_cons(4).any? { |group| group.all? { |cell| cell == symbol } }
+    end
+  end
+
+  def diagonal_win?(symbol)
+    diagonal.any? do |row|
+      row.each_cons(4).any? { |group| group.all? { |cell| cell == symbol } }
+    end
+  end
+
+  def diagonal
+    diags = []
+    (ROWS - 4).times do |r|
+      (COLUMNS - 4).times do |c|
+        diags << [grid[r][c], grid[r + 1][c + 1], grid[r + 2][c + 2], grid[r + 3][c + 3]]
+        # diags << (0..3).map { |i| grid[r + i][c + i] }
+      end
+    end
+
+    (3..ROWS - 1).each do |r|
+      (COLUMNS - 4).times do |c|
+        diags << [grid[r][c], grid[r - 1][c + 1], grid[r - 2][c + 2], grid[r - 3][c + 3]]
+        # diags << (0..3).map { |i| grid[r - i][c + i] }
+      end
+    end
+    diags
   end
 end
--- spec/boardspec.rb
+++ spec/boardspec.rb
@@ -5,4 +5,43 @@ describe Board do
   it 'initialize new board with empty' do
     expect(board.grid.flatten.compact).to be_empty
   end
+
+  it 'place piece lowest empty row' do
+    board.place_piece(0, 'X')
+    expect(board.grid[5][0]).to eq('X')
+  end
+
+  it 'Stacks symbols correctly in a column' do
+    2.times { board.place_piece(0, 'x') }
+    expect(board.grid[4][0]).to eq('x')
+  end
+
+  it 'Raises an error when placed in a full column' do
+    6.times { board.place_piece(0, 'x') }
+    expect { board.place_piece(0, 'x') }.to raise_error('Column is full')
+  end
+
+  it 'Detects a horizontal win' do
+    4.times { |i| board.place_piece(i, 'x') }
+    expect(board.winning_move?('x')).to be true
+  end
+
+  it 'Detects a vertical win' do
+    4.times { board.place_piece(0, 'x') }
+    expect(board.winning_move?('x')).to be true
+  end
+
+  it 'Detects a diagonal win' do
+    board.place_piece(0, 'x')
+    board.place_piece(1, 'o')
+    board.place_piece(1, 'x')
+    board.place_piece(2, 'o')
+    board.place_piece(2, 'o')
+    board.place_piece(2, 'x')
+    board.place_piece(3, 'o')
+    board.place_piece(3, 'o')
+    board.place_piece(3, 'o')
+    board.place_piece(3, 'x')
+    expect(board.winning_move?('x')).to be true
+  end
 end
